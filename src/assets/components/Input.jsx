c0deaaad9f29c708538ed5fae25fadafde866b15
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-17 18:32:36 -0400

    add search icon

--- src/assets/components/Input.jsx
+++ src/assets/components/Input.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Input = () => {
   return (
     <div className="input-container">
+      <FaSearch className="icon" />
       <input type="text" placeholder="Buscar" />
     </div>
   );
