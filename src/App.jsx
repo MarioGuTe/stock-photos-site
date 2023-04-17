958b7a3a7740b0e24f7843ef8e8cbe6190a78ee3
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-17 17:58:41 -0400

    add Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./assets/components/Header";
+import Input from "./assets/components/Input";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
           <h1>FantastiFotos</h1>
           <p>fotografías profesionales a tu alcance</p>
         </div>
+        <Input />
       </div>
     </div>
   );
