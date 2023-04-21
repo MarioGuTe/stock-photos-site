79d631ab7fe4e360cffd87d32906aafdcb760405
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-20 22:54:14 -0400

    update logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,18 @@
 import { useState, useEffect } from "react";
-import Header from "./components/Header";
-import Input from "./components/Input";
-import CategoriesButton from "./components/CategoriesButton";
-import ImageGallery from "./components/ImageGallery";
+import { Routes, Route } from "react-router-dom";
+import DetailsPage from "./pages/DetailsPage";
+import HomePage from "./pages/HomePage";
+
 import "./App.css";
 
 function App() {
-  const [images, setImages] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState("");
-  const apiKey = import.meta.env.VITE_API_KEY;
-
-  const getPhotos = async () => {
-    let response = await fetch(
-      `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&pretty=true`
-    );
-    let data = await response.json();
-    let iterableData = data.hits;
-    setImages(iterableData);
-  };
-
-  useEffect(() => {
-    getPhotos();
-  }, [searchQuery]);
-
   return (
-    <div className="App">
-      <Header />
-      <section className="main-content">
-        <div className="title-section">
-          <h1>FantastiFotos</h1>
-          <p>fotografías profesionales a tu alcance</p>
-        </div>
-        <div className="input-container"></div>
-        <Input searchText={(text) => setSearchQuery(text)} />
-      </section>
-      <section className="categories-section">
-        <h2>Categorías</h2>
-        <div className="buttons-container">
-          <CategoriesButton />
-          <CategoriesButton />
-          <CategoriesButton />
-          <CategoriesButton />
-        </div>
-      </section>
-      <section className="img-section">
-        <h2>Imágenes Royalty Free</h2>
-        <div className="img-gallery-container">
-          {images.map((image) => (
-            <ImageGallery
-              key={image.id}
-              image={image.webformatURL}
-              description={image.tags}
-            />
-          ))}
-        </div>
-      </section>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details" element={<DetailsPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/DetailsPage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const DetailsPage = () => {
+  return (
+    <>
+      <header className="details-header">
+        <div className="go-back-container"></div>
+        <div className="details-input-container">
+          <input type="text" />
+        </div>
+      </header>
+      <section className="details-buttons">
+        <p>heart</p>
+        <div className="details-download-button">
+          <button>Descargar</button>
+          <p>icon</p>
+        </div>
+      </section>
+      <section className="details-img-section">
+        <div className="details-image-container">
+          <img
+            src="https://images.pexels.com/photos/11631883/pexels-photo-11631883.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="hola"
+          />
+        </div>
+        <div className="details-info-container">
+          <p>info 1</p>
+          <p>info 2</p>
+          <p>info 3</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default DetailsPage;
