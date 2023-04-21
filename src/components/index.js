f6f7f5d4e513a6a9e35ae796be82029bdca2f865
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-20 22:51:47 -0400

    add index.js to export components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header";
+import CategoriesButton from "./CategoriesButton";
+import ImageGallery from "./ImageGallery";
+import Input from "./Input";
+
+export default { Header, CategoriesButton, ImageGallery, Input };
