28177265f5617aca150ea95ef2d6205b207855d0
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-21 23:00:44 -0400

    add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import DetailsPage from "./pages/DetailsPage";
 import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/details" element={<DetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>La página que intentas visitar no existe</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
