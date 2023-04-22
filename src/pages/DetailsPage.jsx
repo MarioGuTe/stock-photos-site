c6e94021c9dee1175eed57cb3b5c1b50e9cf5177
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-22 01:47:50 -0400

    update details page layout

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -31,13 +31,16 @@ const DetailsPage = () => {
         <div className="details-image-container">
           <img src={location.state.photo} alt="hola" />
         </div>
-        <div className="details-user-container">
-          <p>Foto cortesía de: {location.state.user}</p>
-          <img src={location.state.userImage} alt="photo-provider" />
-        </div>
         <div className="details-info-container">
           <p>info 2</p>
+          <p>info 2</p>
           <p>info 3</p>
+        </div>
+        <div className="details-user-container">
+          <p>Foto cortesía de: {location.state.user}</p>
+          <div className="user-image-container">
+            <img src={location.state.userImage} alt="photo-provider" />
+          </div>
         </div>
       </section>
     </>
