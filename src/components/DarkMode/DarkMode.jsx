b02c4418853dbfbbe38866f329cc3e7bd2f17dac
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-23 23:49:06 -0400

    add toggle theme logic

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -4,10 +4,25 @@ import { ReactComponent as Moon } from "./Moon.svg";
 import "./DarkMode.css";
 
 const DarkMode = () => {
+  const setDarkMode = () => {
+    document.querySelector("body").setAttribute("data-theme", "dark");
+  };
+  const setLightMode = () => {
+    document.querySelector("body").setAttribute("data-theme", "light");
+  };
+
+  const toggleTheme = (e) => {
+    e.target.checked ? setDarkMode() : setLightMode();
+  };
   return (
     <div className="dark_mode">
-      <input className="dark_mode_input" type="checkbox" id="darkmode-toggle" />
-      <label className="dark_mode_label" for="darkmode-toggle">
+      <input
+        className="dark_mode_input"
+        type="checkbox"
+        id="darkmode-toggle"
+        onChange={toggleTheme}
+      />
+      <label className="dark_mode_label" htmlFor="darkmode-toggle">
         <Sun />
         <Moon />
       </label>
