812d33b06c55ca4160768b9a70e2513ab21c401f
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-24 16:53:26 -0400

    add session storage logic

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -6,15 +6,22 @@ import "./DarkMode.css";
 const DarkMode = () => {
   const setDarkMode = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
+    sessionStorage.setItem("selectedTheme", "dark");
   };
   const setLightMode = () => {
     document.querySelector("body").setAttribute("data-theme", "light");
+    sessionStorage.setItem("selectedTheme", "light");
   };
-
   const toggleTheme = () => {
     const dataTheme = document.querySelector("body").getAttribute("data-theme");
     dataTheme === "light" ? setDarkMode() : setLightMode();
   };
+
+  const selectedTheme = sessionStorage.getItem("selectedTheme");
+  if (selectedTheme === "dark") {
+    setDarkMode();
+  }
+
   return (
     <div className="dark_mode">
       <input
@@ -22,6 +29,7 @@ const DarkMode = () => {
         type="checkbox"
         id="darkmode-toggle"
         onChange={toggleTheme}
+        defaultChecked={selectedTheme === "dark"}
       />
       <label className="dark_mode_label" htmlFor="darkmode-toggle">
         <Sun />
